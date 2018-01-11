3a7fe560a001e9157a1de1d6d87970c29d4b578d
Yury Nevalenny <nevalenny@gmail.com>
2018-01-11 11:21:42 +0300

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,3 +1,3 @@
-/usr/bin/xcodebuild -destination platform=\"iOS Simulator,name=iPhone 6\" -workspace $APPCENTER_SOURCE_DIRECTORY/ios/DemoApp.xcworkspace -scheme DemoApp clean archive CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=NO -archivePath $APPCENTER_OUTPUT_DIRECTORY/build/archive/DemoApp.xcarchive
+/usr/bin/xcodebuild -destination platform="iOS Simulator,name=iPhone 6" -workspace $APPCENTER_SOURCE_DIRECTORY/ios/DemoApp.xcworkspace -scheme DemoApp clean archive CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=NO -archivePath $APPCENTER_OUTPUT_DIRECTORY/build/archive/DemoApp.xcarchive
 rm -rf $BUILD_ARTIFACTSTAGINGDIRECTORY/*
 cp -R $APPCENTER_OUTPUT_DIRECTORY/build/archive/DemoApp.xcarchive $BUILD_ARTIFACTSTAGINGDIRECTORY/build
